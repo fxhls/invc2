9fed35d1fda62ee28cebed3f460a9f23a6672608
fxhls <50634782+fxhls@users.noreply.github.com>
2022-01-23 08:57:53 -0500

    p

--- test.js
+++ test.js
@@ -1,12 +1,23 @@
-// var ChildProcess = require('child_process');
-// const exec = ChildProcess.execFile;
+var ChildProcess = require('child_process');
+const exec = ChildProcess.execFile;
+var path = require('path');
+var exePath = path.resolve(__dirname, './src');
 
-// var fun =function(){
-//    console.log("fun() start");
-//    exec('src', function(err, data) {  
-//         console.log(err)
-//         console.log(data.toString());                       
-//     });  
+
+var fun =function(){
+   console.log("fun() start");
+   exec(exePath, function(err, data) {  
+        console.log(err)
+        console.log(data.toString());                       
+    });  
+}
+fun();
+
+// var fun = function () {
+//     spawn('node', ['src'], {
+//         env: {
+//             NODE_ENV: 'production',
+//             PATH: process.env.PATH
+//         }
+//     })
 // }
-// fun();
-console.log('hi')

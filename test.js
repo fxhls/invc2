38427d9d9234bbbed985b570af52c46459e5fbab
fxhls <50634782+fxhls@users.noreply.github.com>
2022-01-23 08:25:33 -0500

    h

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-var ChildProcess = require('child_process');
-const exec = ChildProcess.execFile;
+// var ChildProcess = require('child_process');
+// const exec = ChildProcess.execFile;
 
-var fun =function(){
-   console.log("fun() start");
-   exec('src', function(err, data) {  
-        console.log(err)
-        console.log(data.toString());                       
-    });  
-}
-fun();
+// var fun =function(){
+//    console.log("fun() start");
+//    exec('src', function(err, data) {  
+//         console.log(err)
+//         console.log(data.toString());                       
+//     });  
+// }
+// fun();
+console.log('hi')
